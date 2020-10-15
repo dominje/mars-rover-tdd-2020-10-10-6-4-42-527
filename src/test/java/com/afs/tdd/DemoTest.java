06d76e5951813f1d35bd5722ad5be36608f18c34
dominje <jericah.domingo@oocl.com>
2020-10-15 18:55:33 +0800

    [marsRover][jericah]test: add test that will output 00W when the input is 00NL

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -19,5 +19,20 @@ class DemoTest {
         assertEquals("01N",actual);
     }
 
+    @Test
+    void should_return_x_0_y_0_direction_W_when_x_0_y_0_direction_N_given_command_L(){
+        // given
+        int xAxis = 0;
+        int yAxis = 0;
+        char direction = 'W';
+        String command = String.valueOf('L');
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        // when
+        String actual = marsRover.executeCommand();
+
+        // then
+        assertEquals("00W",actual);
+        
+    }
 
 }
