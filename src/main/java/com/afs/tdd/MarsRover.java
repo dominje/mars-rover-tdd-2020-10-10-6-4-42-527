e1c85263a6fa61b089677b9468fd8a7e2ebd5dfb
dominje <jericah.domingo@oocl.com>
2020-10-15 17:58:56 +0800

    [marsRover][jericah]feature: added initial code for execute command

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,46 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    private int xAxis;
+    private int yAxis;
+    private char direction;
+    private String command;
+
+    public MarsRover(int xAxis, int yAxis, char direction, String command) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        this.direction = direction;
+        this.command = command;
+    }
+
+    public String executeCommand() {
+        if(command.equals("M")){
+            moveForward();
+        }
+        return String.valueOf(xAxis + yAxis + direction);
+    }
+
+    public void moveForward(){
+        if(String.valueOf(direction).equals("N")){
+            yAxis += 1;
+        }
+    }
+
+    public int getxAxis() {
+        return xAxis;
+    }
+
+    public int getyAxis() {
+        return yAxis;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+}
