699c4a45da7e563806b943ceba3796f1a34a4e3a
dominje <jericah.domingo@oocl.com>
2020-10-15 17:57:28 +0800

    [marsRover][jericah]refactor: refactor test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -7,15 +7,17 @@ class DemoTest {
     @Test
     void should_return_x_0_y_1_direction_N_when_x_0_y_0_direction_N_given_command_M() {
         // given
-        MarsRover marsRover = new MarsRover();
         int xAxis = 0;
         int yAxis = 0;
         char direction = 'N';
-        char command = 'M';
+        String command = String.valueOf('M');
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
         // when
-        String actual = marsRover.executeCommand(xAxis, yAxis, direction, command);
+        String actual = marsRover.executeCommand();
 
         // then
         assertEquals("01N",actual);
     }
+
+
 }
