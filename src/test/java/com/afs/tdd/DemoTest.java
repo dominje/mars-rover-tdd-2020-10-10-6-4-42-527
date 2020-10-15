f565742451e3ac78ff4720878587dbddd745c378
dominje <jericah.domingo@oocl.com>
2020-10-15 17:06:37 +0800

    [marsRover][jericah]test: add test that will output 01N when the input is 00NM

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,21 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_x_0_y_1_direction_N_when_x_0_y_0_direction_N_given_command_M() {
+        // given
+        MarsRover marsRover = new MarsRover();
+        int xAxis = 0;
+        int yAxis = 0;
+        char direction = 'N';
+        char command = 'M';
+        // when
+        String actual = marsRover.executeCommand(xAxis, yAxis, direction, command);
 
+        // then
+        assertEquals("01N",actual);
     }
 }
