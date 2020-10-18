b94efb721548bc91be09798a83650da4abe61eee
dominje <jericah.domingo@oocl.com>
2020-10-18 14:35:21 +0800

    [MarsRover][jericah]refactor: changed name from DemoTest to MarsRoverTest.java

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -1,10 +1,11 @@
 package com.afs.tdd;
 
+import com.afs.tdd.exception.CommandNotDefinedException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DemoTest {
+class MarsRoverTest {
 
     int xAxis = 0;
     int yAxis = 0;
@@ -12,38 +13,38 @@ class DemoTest {
     @Test
     void should_return_x_0_y_1_direction_N_when_x_0_y_0_direction_N_given_command_M() throws CommandNotDefinedException {
         // given
-        char direction = 'N';
+        Direction direction = Direction.valueOf("NORTH");
         String command = "M";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("01N",actual);
     }
-
     @Test
     void should_return_x_0_y_0_direction_W_when_x_0_y_0_direction_N_given_command_L() throws CommandNotDefinedException {
         // given
-        char direction = 'N';
+        Direction direction = Direction.valueOf("NORTH");
         String command = "L";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00W",actual);
-        
+
     }
 
     @Test
     void should_return_x_0_y_0_direction_E_when_x_0_y_0_direction_N_given_command_R() throws CommandNotDefinedException {
         // given
-        char direction = 'N';
+        Direction direction = Direction.valueOf("NORTH");
         String command = "R";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00E",actual);
@@ -53,11 +54,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_negative1_direction_S_when_x_0_y_0_direction_S_given_command_M() throws CommandNotDefinedException {
         // given
-        char direction = 'S';
+        Direction direction = Direction.valueOf("SOUTH");
         String command = "M";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("0-1S",actual);
@@ -67,11 +68,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_E_when_x_0_y_0_direction_S_given_command_L() throws CommandNotDefinedException {
         // given
-        char direction = 'S';
+        Direction direction = Direction.valueOf("SOUTH");
         String command = "L";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00E",actual);
@@ -81,11 +82,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_W_when_x_0_y_0_direction_S_given_command_R() throws CommandNotDefinedException {
         // given
-        char direction = 'S';
+        Direction direction = Direction.valueOf("SOUTH");
         String command = "R";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00W",actual);
@@ -95,11 +96,11 @@ class DemoTest {
     @Test
     void should_return_x_1_y_0_direction_E_when_x_0_y_0_direction_E_given_command_M() throws CommandNotDefinedException {
         // given
-        char direction = 'E';
+        Direction direction = Direction.valueOf("EAST");
         String command = "M";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("10E",actual);
@@ -109,11 +110,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_N_when_x_0_y_0_direction_E_given_command_L() throws CommandNotDefinedException {
         // given
-        char direction = 'E';
+        Direction direction = Direction.valueOf("EAST");
         String command = "L";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00N",actual);
@@ -123,11 +124,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_S_when_x_0_y_0_direction_E_given_command_R() throws CommandNotDefinedException {
         // given
-        char direction = 'E';
+        Direction direction = Direction.valueOf("EAST");
         String command = "R";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00S",actual);
@@ -137,11 +138,11 @@ class DemoTest {
     @Test
     void should_return_x_negative1_y_0_direction_W_when_x_0_y_0_direction_W_given_command_M() throws CommandNotDefinedException {
         // given
-        char direction = 'W';
+        Direction direction = Direction.valueOf("WEST");
         String command = "M";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("-10W",actual);
@@ -151,11 +152,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_S_when_x_0_y_0_direction_W_given_command_L() throws CommandNotDefinedException {
         // given
-        char direction = 'W';
+        Direction direction = Direction.valueOf("WEST");
         String command = "L";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00S",actual);
@@ -165,11 +166,11 @@ class DemoTest {
     @Test
     void should_return_x_0_y_0_direction_N_when_x_0_y_0_direction_W_given_command_R() throws CommandNotDefinedException {
         // given
-        char direction = 'W';
+        Direction direction = Direction.valueOf("WEST");
         String command = "R";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("00N",actual);
@@ -179,14 +180,14 @@ class DemoTest {
     @Test
     void should_throw_command_not_defined_exception_when_x_0_y_0_direction_N_given_command_F() throws CommandNotDefinedException {
         // given
-        char direction = 'N';
+        Direction direction = Direction.valueOf("NORTH");
         String command = "F";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
 
         CommandNotDefinedException thrown = assertThrows(
                 CommandNotDefinedException.class,
-                () -> marsRover.executeCommand(),
+                () -> marsRover.executeCommand(command),
                 "Expected marsRover() to throw, but it didn't"
         );
 
@@ -198,11 +199,11 @@ class DemoTest {
     @Test
     void should_return_x_negative1_y_negative1_direction_N_when_x_0_y_0_direction_N_given_command_MLMR() throws CommandNotDefinedException {
         // given
-        char direction = 'N';
-        String command = "MLMR";
-        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction, command);
+        Direction direction = Direction.valueOf("NORTH");
+        String command = "M,L,M,R";
+        MarsRover marsRover = new MarsRover(xAxis, yAxis, direction);
         // when
-        String actual = marsRover.executeCommand();
+        String actual = marsRover.executeCommand(command);
 
         // then
         assertEquals("-11N",actual);
