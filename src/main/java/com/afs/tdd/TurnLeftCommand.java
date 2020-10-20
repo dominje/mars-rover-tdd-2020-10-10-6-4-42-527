2117b3761f51f736cf035eee03da2cbd4282eee5
dominje <jericah.domingo@oocl.com>
2020-10-20 21:51:10 +0800

    [MarsRover][jericah]feature: added command class: TurnLeftCommand

---
+++ src/main/java/com/afs/tdd/TurnLeftCommand.java
@@ -0,0 +1,16 @@
+package com.afs.tdd;
+
+public class TurnLeftCommand implements Command{
+
+    MarsRover marsRover;
+
+    public TurnLeftCommand(MarsRover marsRover) {
+        this.marsRover = marsRover;
+    }
+
+    @Override
+    public void execute() {
+        marsRover.turnLeft();
+    }
+
+}
